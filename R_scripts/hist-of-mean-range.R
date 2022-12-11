79bbd4d7270d3ea3ad8b0ec2a2153576384f983b
Frank Saunders Jr <112049118+FranksCodes@users.noreply.github.com>
2022-12-11 22:42:54 +0900

    Create hist-of-mean-range.R

--- R_scripts/hist-of-mean-range.R
+++ R_scripts/hist-of-mean-range.R
@@ -1,12 +1,11 @@
-setwd("/Users/Frankzi/Documents/data/pgr-visualization")
+#This was used to generate the histogram based on range_dist.csv, generated from range_avg_percent.sql. 
+
 range_dist <- read.csv("range_dist.csv")
 ggplot(range_dist, aes(x=mean_range)) + 
   geom_histogram(aes(y = ..density..), binwidth=.1, fill="#69b3a2")+
   geom_density(alpha=.3, fill="#FF6666") +
   theme_bw()+
   xlab("Range of Mean")+
-#  stat_bin(aes(y=..count.., label=..count..), geom="text")+
-#  coord_cartesian(xlim = c(0,1), ylim = c(0, 26)) +
   ylab("Density")+
   labs(title = "Mean Score Fluctuation", 
        subtitle="Histogram of Range of Overall Mean Scores",
